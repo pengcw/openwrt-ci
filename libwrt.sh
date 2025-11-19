5f25baad78bde1a29010f9e887bd561b569a18ae
pengcw <pydove@foxmail.com>
2025-11-19 15:48:39 +0800

    optimize

--- libwrt.sh
+++ libwrt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# 
+rm -rf package/emortal/luci-app-athena-led
+# 去除值守管理
 rm -rf package/emortal/luci-app-athena-led
 
 # Git稀疏克隆，只克隆指定目录到本地
@@ -16,3 +17,5 @@ git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 # 添加momo
 git_sparse_clone main https://github.com/kenzok8/small-package momo luci-app-momo
+#添加chinadns
+git clone -b master https://github.com/izilzty/luci-app-chinadns-ng package/luci-app-chinadns-ng
